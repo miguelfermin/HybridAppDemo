ed0a9afa9483df801d855fb689b7a5e433b4620d
Miguel Fermin <mfermineet@gmail.com>
2014-11-24 11:27:23 -0500

    Removed code that is irrelevant to the "Roll Call" demo app. Changed story view's back button to look more like the Roll Call app

--- www/js/app.js
+++ www/js/app.js
@@ -21,7 +21,6 @@ app.run(function($ionicPlatform) {
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
-
   // Fallback state
   $urlRouterProvider.otherwise('/app/stories-search');
 
@@ -31,9 +30,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
       abstract: true,
       templateUrl: "templates/menu.html"
   });
-
-
-
 
   // Stories Search
   $stateProvider.state('app.stories-search', {
@@ -65,59 +61,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
     resolve: {
       story: function($stateParams, StoriesSearchService) {
         return StoriesSearchService.getStory($stateParams.story);
-      }
-    }
-  });
-
-
-
-  /* NOTE: Please ignore the states below. They are not being used for now. */
-  
-  // Show all stories from ACI, using paging and StoriesService
-  $stateProvider.state('app.stories', {
-    abstract: true,
-    url: '/stories',
-    views: {
-      'menuContent': {
-        template: '<ion-nav-view></ion-nav-view>'
-      }
-    }
-  });
-  $stateProvider.state('app.stories.index', {
-    url: '',
-    templateUrl:'templates/stories.html',
-    controller: 'StoriesController',
-    // Keep data on state to try to avoid the master list to reload when coming back from detail view.
-    // data: {
-    //   stories:[]
-    // }
-  });
-  $stateProvider.state('app.stories.detail', {
-    url: '/:story',
-    templateUrl:'templates/story.html',
-    controller: 'StoryController',
-    resolve: {
-      story: function($stateParams, StoriesService) {
-        return StoriesService.getStory($stateParams.story);
-      }
-    }
-  });
-  // Search Filter method, without using a service, all work done in the controller. Kept for reference only
-  $stateProvider.state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent' :{
-        templateUrl: "templates/search-filter.html",
-        controller: 'SearchFilterCtrl'
-      }
-    }
-  });
-  $stateProvider.state('app.searchDetail', {
-    url: "/search/:pub_id?story_title&story_text&authors",
-    views: {
-      'menuContent' :{
-        templateUrl: "templates/story.html",
-        controller: 'SearchFilterDetailCtrl'
       }
     }
   });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -131,100 +131,19 @@ app.controller('ActiveSearchBarController', function($scope, $location, $rootSco
   }
 });
 
-app.controller('PassiveSearchBarController', function($scope, $state, $rootScope) {
-  $scope.shouldShowPassiveSearchBar = function () {
-    return $state.current.data.shouldShowPassiveSearchBar;
-  };
-
-  $scope.showActiveSearchBar = function() {
-    $rootScope.$broadcast('showActiveSearchBarInvoked');
-    $state.current.data.shouldShowPassiveSearchBar = false;
-  };
-
-  $scope.$on('activeSearchBarWasHidden', function(event, query) {
-    $state.current.data.shouldShowPassiveSearchBar = true;
-  });
-});
-
 app.controller('StoryController', function($scope, story, $timeout, $ionicNavBarDelegate) {
   $scope.story = story;
 });
 
-/* NOTE: Please ignore the controllers below. They are not being used for now. */
-app.controller('SearchFilterCtrl', function($scope, $http, $ionicScrollDelegate) {
-  // Imperfect stories load code, still using it for the show the search filer
-  $scope.numberOfPages  = 0;
-  $scope.pageCounter = 0;
-  $scope.storyCounter = 0;
-  $scope.stories = [];
-  $scope.loadMore = function() {
-    var params = {page: $scope.pageCounter.toString()};
-    console.log('$scope.pageCounter: ' + $scope.pageCounter);
-    $scope.pageCounter += 1;
-    $http({ method: 'GET', url: 'http://dev.acindex.com/search', params: params })
-      .success(function(data, status, headers) {
-        if (data && data.hits && data.hits.length > 0) {
-          $scope.hits_count = data.hits.length;
-          if ($scope.numberOfPages === 0) $scope.numberOfPages = data.nbPages;
-          data.hits.forEach(function(hit) {
-            $scope.storyCounter += 1;
-            var story = {
-              number: $scope.storyCounter,
-              title: hit.title,
-              date: hit.date,
-              pub_id: hit.pub.$id,
-              pub_name: hit.pub.name,
-              authors: hit.authors
-            };
-            if (hit._highlightResult.text) {story.text = hit._highlightResult.text.value;}
-            else {story.text = '_highlightResult.text was null';}
-            $scope.stories.push(story);
-          });
-          $scope.$broadcast('scroll.infiniteScrollComplete');
-          $ionicScrollDelegate.scrollToRememberedPosition();
-        }
-      })
-      .error(function(error, code) {alert("Error: " + error + ', code: ' + code);});
-  };
-  $scope.filterSearch = function() {
-    // This needs improvements; it isn't dynamic, when user deletes words it doesn't reload list...
-    console.log('$scope.search: ' + $scope.search);
-    $scope.stories = $scope.stories
-    // The filter() method creates a new array with all elements that pass the test implemented by the provided function.
-    .filter(function(item) {
-      console.log('item: ' + item.title);
-      // The indexOf() method returns the index within the calling String object of the first occurrence 
-      // of the specified value, starting the search at fromIndex. Returns -1 if the value is not found.
-      return (!$scope.search || item.title.toLowerCase().indexOf($scope.search.toLowerCase()) > -1);
-    });
-  };
-});
-app.controller('SearchFilterDetailCtrl', function($scope, $stateParams) {
-  $scope.pub_id = $stateParams.pub_id;
-  $scope.story_title = $stateParams.story_title;
-  $scope.story_text = $stateParams.story_text;
-});
-app.controller('StoriesController', function($scope, $ionicScrollDelegate, StoriesService) {
-
-  $scope.loadMoreStories = function() {
-    // Handle StoriesService's getStories() returned promise
-    StoriesService.getStories().then(
-    function(stories) {
-      $ionicScrollDelegate.scrollToRememberedPosition();
-      $scope.stories = stories;
-
-      $scope.$broadcast('scroll.infiniteScrollComplete');
-      $ionicScrollDelegate.scrollToRememberedPosition();
-    },
-    function(failedInfo) {
-      alert('failedInfo: ' + failedInfo);
-    });
-  };
-});
-// Experimental code
-// console.log('$location.path() ' + $location.path());
-// $location.search( { q: $scope.query } );
-// $scope.$on('$locationChangeSuccess', function() {
-//   console.log('locationChangeSuccess');
-//   updateSearchFromURL();
+// app.controller('PassiveSearchBarController', function($scope, $state, $rootScope) {
+//   $scope.shouldShowPassiveSearchBar = function () {
+//     return $state.current.data.shouldShowPassiveSearchBar;
+//   };
+//   $scope.showActiveSearchBar = function() {
+//     $rootScope.$broadcast('showActiveSearchBarInvoked');
+//     $state.current.data.shouldShowPassiveSearchBar = false;
+//   };
+//   $scope.$on('activeSearchBarWasHidden', function(event, query) {
+//     $state.current.data.shouldShowPassiveSearchBar = true;
+//   });
 // });
